9d6ead75537e63dd8f2c2ff1052a8e8522875c9d
Yashika.U <97971705+Yashika24-U@users.noreply.github.com>
2023-09-09 10:43:57 +0530

    Add files via upload

---
+++ company.js
@@ -0,0 +1,41 @@
+const tablebody = document.querySelector("tbody");
+const fun1 = async (company) => {
+  const response = await fetch(
+   
+    `https://companyenrichment.abstractapi.com/v1/?api_key=a742b1d68f574c7ab945c7fe19ef1f71&domain=${company}.com`
+  );
+  const value = await response.json();
+  console.log(value);
+
+  const bodyrow = document.createElement("tr");
+  const bodycomp = document.createElement("td");
+  bodycomp.innerText = value.name;
+  if (bodycomp.innerText == "") {
+    window.alert("company not present in API");
+  } else {
+    const bodyFound = document.createElement("td");
+    bodyFound.innerText = value.year_founded;
+
+    const bodyHq = document.createElement("td");
+    bodyHq.innerText = value.country + ` ( ${value.locality} )`;
+
+    const bodyDomain = document.createElement("td");
+    bodyDomain.innerText = value.domain;
+
+    const bodyempCount = document.createElement("td");
+    bodyempCount.innerText = value.employees_count;
+
+    bodyrow.append(bodycomp, bodyFound, bodyHq, bodyDomain, bodyempCount);
+    tablebody.append(bodyrow);
+  }
+};
+
+const formEle = document.getElementById("form");
+formEle.addEventListener("submit", (e) => {
+  e.preventDefault();
+  tablebody.innerText = "";
+  const element = e.target.elements;
+  const inputEle = document.getElementById("company");
+  console.log(inputEle.value);
+  fun1(inputEle.value);
+});
---
+++ emailValidation.js
@@ -0,0 +1,29 @@
+const btn = document.createElement("button");
+const fun1 = async (email) => {
+  const valu = await fetch(
+    // `https://emailvalidation.abstractapi.com/v1/?api_key=95ae6cbe3fc04e9c94791562eabdaf0d&email=${email}`
+    `https://emailvalidation.abstractapi.com/v1/?api_key=d14374922f8241178a51c960b04ebc74&email={email}.com`
+  
+  );
+  const result = await valu.json();
+
+  const displaytext = result.deliverability;
+  btn.style.display = "block";
+  btn.innerHTML = displaytext;
+  console.log(displaytext);
+  if (displaytext == "DELIVERABLE") {
+    btn.setAttribute("class", "btn btn-primary");
+  } else {
+    btn.setAttribute("class", "btn btn-danger");
+  }
+  document.body.append(btn);
+};
+
+const inputEle = document.getElementById("form");
+
+inputEle.addEventListener("submit", (e) => {
+  btn.style.display = "none";
+  e.preventDefault();
+  const inputValue = document.getElementById("email");
+  fun1(inputValue.value);
+});
